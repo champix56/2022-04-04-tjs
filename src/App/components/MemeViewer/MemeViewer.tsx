3d812e0f0114ed045544d7775853b9249b225540
champix56 <desorbaix@free.fr>
2022-04-05 10:10:55 +0200

    svg complet et fonctionnel

--- src/App/components/MemeViewer/MemeViewer.tsx
+++ src/App/components/MemeViewer/MemeViewer.tsx
@@ -5,21 +5,31 @@ import { I_Image, I_Meme } from "../../interfaces/common";
 
 interface I_MemeVieverProps {
   meme: I_Meme;
-  image: I_Image|undefined;
+  image: I_Image | undefined;
 }
 const MemeViewer: React.FC<I_MemeVieverProps> = (props) => {
   return (
-    <svg className={style.MemeViewer} data-testid="MemeViewer">
-      {undefined!==props.image?<image href={`/img/${props.image.url}`} x="0" y="0" />:null}
+    <svg
+      className={style.MemeViewer}
+      data-testid="MemeViewer"
+      viewBox={`0 0 ${props.image ? props.image.w : "1000"} ${
+        props.image ? props.image.h : "1000"
+      }`}
+    >
+      {undefined !== props.image && (
+        <image href={`/img/${props.image.url}`} x="0" y="0" />
+      )}
       <text
         x={props.meme.x}
         y={props.meme.y}
         fontSize={props.meme.fontSize}
         fill={props.meme.color}
-        textDecoration={props.meme.underline?'underline':'none'}
-        fontStyle={props.meme.italic?'italic':'normal'}
+        textDecoration={props.meme.underline ? "underline" : "none"}
+        fontStyle={props.meme.italic ? "italic" : "normal"}
         fontWeight={props.meme.fontWeight}
-      ></text>
+      >
+        {props.meme.text}
+      </text>
     </svg>
   );
 };
