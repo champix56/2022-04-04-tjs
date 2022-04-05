2c11e8275479ccb4882cbb7b463394c3b4f364c7
champix56 <desorbaix@free.fr>
2022-04-05 14:35:51 +0200

    two way binding  sur les inputs

--- src/App/components/MemeForm/MemeForm.tsx
+++ src/App/components/MemeForm/MemeForm.tsx
@@ -13,37 +13,84 @@ const MemeForm: FC<I_MemeFormProps> = (props) => {
     <div data-testid="MemeForm" className={styles.MemeForm}>
       <form>
         <h1>Titre</h1>
-        <input type="text" id="f_titre" placeholder="saisir titre" />
+        <input
+          type="text"
+          id="f_titre"
+          placeholder="saisir titre"
+          value={props.currentMeme.titre}
+          onChange={(evt) => {
+            props.onInputValueChange({ titre: evt.target.value });
+          }}
+        />
         <hr />
         <h2>Image</h2>
-        <select>
+        <select
+          value={props.currentMeme.imageId}
+          onChange={(evt) => {
+            props.onInputValueChange({ imageId: Number(evt.target.value) });
+          }}
+        >
           <option value="-1">Aucune</option>
         </select>
         <hr />
         <h2>text</h2>
-        <input type="text" value={props.currentMeme.text} onChange={
-          evt=>{props.onInputValueChange({text:evt.target.value})}
-        } />
+        <input
+          type="text"
+          value={props.currentMeme.text}
+          onChange={(evt) => {
+            props.onInputValueChange({ text: evt.target.value });
+          }}
+        />
         <div className={styles.half}>
           <div>
             <label htmlFor="f_x">x:</label>
             <br />
-            <input type="number" className={styles.smallInput} />
+            <input
+              type="number"
+              className={styles.smallInput}
+              value={props.currentMeme.x}
+              onChange={(evt) => {
+                props.onInputValueChange({ x: Number(evt.target.value) });
+              }}
+            />
           </div>
           <div>
             <label htmlFor="f_y">y:</label>
             <br />
-            <input type="number" className={styles.smallInput} />
+            <input
+              type="number"
+              className={styles.smallInput}
+              value={props.currentMeme.y}
+              onChange={(evt) => {
+                props.onInputValueChange({ y: Number(evt.target.value) });
+              }}
+            />
           </div>
         </div>
         <hr />
         <label htmlFor="f_color">Couleur</label>
-        <input type="color" id="f_color"  />
+        <input
+          type="color"
+          id="f_color"
+          value={props.currentMeme.color}
+          onChange={(evt) => {
+            props.onInputValueChange({ color: evt.target.value });
+          }}
+        />
         <div className={styles.half}>
           <div>
             <label htmlFor="f_sz">font-size:</label>
             <br />
-            <input type="number" className={styles.smallInput} />
+            <input
+              type="number"
+              className={styles.smallInput}
+              value={props.currentMeme.fontSize}
+              onChange={(evt) => {
+                props.onInputValueChange({
+                  fontSize: Number(evt.target.value),
+                });
+              }}
+            />
           </div>
           <div>
             <label htmlFor="f_fw">font-weight:</label>
@@ -54,6 +101,10 @@ const MemeForm: FC<I_MemeFormProps> = (props) => {
               min="100"
               step="100"
               max="900"
+              value={props.currentMeme.fontWeight}
+              onChange={(evt) => {
+                props.onInputValueChange({ fontWeight: evt.target.value });
+              }}
             />
           </div>
         </div>
@@ -64,6 +115,10 @@ const MemeForm: FC<I_MemeFormProps> = (props) => {
             <input
               id="f_underline"
               type="checkbox"
+              checked={props.currentMeme.underline}
+              onChange={(evt) => {
+                props.onInputValueChange({ underline: evt.target.checked });
+              }}
             />
           </div>
           <div>
@@ -72,6 +127,10 @@ const MemeForm: FC<I_MemeFormProps> = (props) => {
             <input
               id="f_italic"
               type="checkbox"
+              checked={props.currentMeme.italic}
+              onChange={(evt) => {
+                props.onInputValueChange({ italic: evt.target.checked });
+              }}
             />
           </div>
         </div>
