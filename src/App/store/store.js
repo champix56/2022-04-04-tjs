2d8b00c746e2395eb3e0b81ec3f2ca8bd718ae16
champix56 <desorbaix@free.fr>
2022-04-05 15:39:20 +0200

    initial async loading

--- src/App/store/store.js
+++ src/App/store/store.js
@@ -1,4 +1,5 @@
 import {createStore} from 'redux'
+import { REST_SRV_BASE_URL } from '../config/config';
 const initialRessourcesState={
     images:[],
     memes:[],
@@ -14,6 +15,14 @@ function ressourcesReducer(state=initialRessourcesState,action){
         case RessourcesActions.ADD_INIT_IMAGES:return {...state,images:action.values}
         case RessourcesActions.ADD_INIT_MEMES:return {...state,memes:action.values}
         case RessourcesActions.ADD_MEME:return {...state,memes:[...state.memes,action.value]}
+        case 'ADD_INIT_ALL':return {...state,memes:action.memes,images:action.images}
+        case 'INIT_LOADING':
+            const prm = fetch(`${REST_SRV_BASE_URL}/memes`).then((f) => f.json());
+            const pri = fetch(`${REST_SRV_BASE_URL}/images`).then((f) => f.json());
+            Promise.all([prm, pri]).then((aResp) => {
+              store.dispatch({type:'ADD_INIT_ALL',memes:aResp[0],images:aResp[1]});
+            });
+            return state;
         default: return state;
     }
 }
@@ -21,6 +30,7 @@ export const store=createStore(ressourcesReducer);
 store.subscribe(()=>{
     console.log(store.getState())
 });
+store.dispatch({type:'INIT_LOADING'});
 // store.dispatch({type:RessourcesActions.ADD_INIT_IMAGES,values:[{id:0},{id:1}]})
 // store.dispatch({type:RessourcesActions.ADD_INIT_MEMES,values:[{id:10},{id:21}]})
 // store.dispatch({type:RessourcesActions.ADD_MEME,value:{id:123}});
