7a5cfe4629579a61a141c9084a2493b55312af36
champix56 <desorbaix@free.fr>
2022-04-06 12:07:11 +0200

    composant de selection et meo de l'editor

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 import style from "./App.module.css";
 import FlexHLayout from "./components/layouts/FlexHLayout/FlexHLayout";
 import FlexWLayout from "./components/layouts/FlexWLayout/FlexWLayout";
@@ -7,6 +7,9 @@ import MemeThumbnail from "./components/MemeThumbnail/MemeThumbnail";
 import MemeViewer from "./components/MemeViewer/MemeViewer";
 import Navbar from "./components/Navbar/Navbar";
 import { Route, Switch, Link, useParams, useLocation, useHistory, withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { DummyMeme, I_Meme } from "./interfaces/common";
+import { CURRENT_ACTIONS } from "./store/store";
 
 interface I_AppProps {
   AppName?: string;
@@ -45,6 +48,12 @@ class App extends Component<I_AppProps> {
 }
 function Editor(props:any) {
   console.log(props);
+  useEffect(() => {
+    props.update(props.memes.find((m:I_Meme)=>m.id===parseInt(props.match.params.id)))
+    return () => {
+      props.update(undefined);
+    };
+  }, [props])
   return (
     <FlexWLayout>
       <div>
@@ -54,5 +63,13 @@ function Editor(props:any) {
     </FlexWLayout>
   );
 }
-const RoutedEditor=withRouter(Editor);
+function mstp(state:any,own:any){
+  return {...own,memes:state.ressources.memes}
+}
+function mdtp(dispatch:Function){return {
+  update:(meme:I_Meme|undefined)=>{
+    dispatch({type:CURRENT_ACTIONS.UPDATE_CURRENT,value:(meme?meme:DummyMeme)})
+  }
+}}
+const RoutedEditor=withRouter(connect(mstp,mdtp)(Editor));
 export default App;
