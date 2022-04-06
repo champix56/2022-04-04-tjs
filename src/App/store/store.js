e809907091d920bfc65b03c7f2e6b9548ec4e7a2
champix56 <desorbaix@free.fr>
2022-04-06 10:02:33 +0200

    auto-reformat

--- src/App/store/store.js
+++ src/App/store/store.js
@@ -61,17 +61,18 @@ function currentReducer(state = DummyMeme, action) {
     case CURRENT_ACTIONS.UPDATE_CURRENT:
       return { ...state, ...action.value };
 
-      case CURRENT_ACTIONS.SAVE_CURRENT:
-          fetch(`${REST_SRV_BASE_URL}/memes`,{
-              method:'POST',
-              headers:{
-                  "Content-Type":"application/json"
-              },
-              body:JSON.stringify(state)
-          }).then(f=>f.json())
-          .then(o=>{
-            store.dispatch({type:RessourcesActions.ADD_MEME,value:o})
-          })
+    case CURRENT_ACTIONS.SAVE_CURRENT:
+      fetch(`${REST_SRV_BASE_URL}/memes`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(state),
+      })
+        .then((f) => f.json())
+        .then((o) => {
+          store.dispatch({ type: RessourcesActions.ADD_MEME, value: o });
+        });
       return state;
     default:
       return state;
